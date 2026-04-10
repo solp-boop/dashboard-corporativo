8f2590fcd98a6d0c985a75b3664665ad65c60f02
solp-boop <sol.p@bidcom.com.ar>
2026-04-10 19:01:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-   import streamlit as st
-    import pandas as pd
+import streamlit as st
+import pandas as pd
     import plotly.express as px
     from datetime import datetime, timedelta
     import time
